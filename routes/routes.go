12de97791f80789c0b0f66cce228807fe5f208e9
Lucas Martins <lucas.martins@reclameaqui.com.br>
2023-02-23 10:46:45 -0300

    novo roteador

--- routes/routes.go
+++ routes/routes.go
@@ -4,11 +4,13 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/gorilla/mux"
 	"github.com/rukasugarushia/api-go-rest/controllers"
 )
 
 func HandleRequest() {
-	http.HandleFunc("/", controllers.Home)
-	http.HandleFunc("/api/personalidades", controllers.TodasPersonalidades)
-	log.Fatal(http.ListenAndServe(":8000", nil))
+	r := mux.NewRouter()
+	r.HandleFunc("/", controllers.Home)
+	r.HandleFunc("/api/personalidades", controllers.TodasPersonalidades)
+	log.Fatal(http.ListenAndServe(":8000", r))
 }
