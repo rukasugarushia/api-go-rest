1d2743615ac5385637296b3937d923dcc520ca50
Lucas Martins <lucas.martins@reclameaqui.com.br>
2023-02-23 10:39:27 -0300

    respondendo json

--- main.go
+++ main.go
@@ -3,10 +3,16 @@ package main
 import (
 	"fmt"
 
+	"github.com/rukasugarushia/api-go-rest/models"
 	"github.com/rukasugarushia/api-go-rest/routes"
 )
 
 func main() {
+	models.Personalidades = []models.Personalidade{
+		{Nome: "Nome 1", Historia: "Historia 1"},
+		{Nome: "Nome 2", Historia: "Historia 2"},
+	}
+
 	fmt.Println("Iniciando o servidor Rest com Go")
 	routes.HandleRequest()
 }
